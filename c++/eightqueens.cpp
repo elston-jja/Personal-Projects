1beedd4155ca97ead92b932a394efbb5dbcf86b9
elston <epeglab@gmail.com>
2016-08-24 20:01:03 -0400

    works for one loop and does not provide proper solutions, but fills up the board and does not violate queen rules

--- c++/eightqueens.cpp
+++ c++/eightqueens.cpp
@@ -5,33 +5,42 @@ void deleteGameboard(char**gameboard, int square);
 void printGameboard(char**gameboard, int square);
 void createQueen(char**gameboard, int squares, int x, int y);
 bool checkMove(char**gameboard, int x, int y);
+void checkQueens(char**gameboard, int squares);
 
 int main() {
 
-  int input, x(0), y(0),a ;
-  bool valid;
+  int input, y(0);
 
   std::cout << "Amount of queens: ";
   std::cin >> input;
 
   char** gameboard = createEmptyGameboard(input);
-  printGameboard(gameboard, input);
-
-  createQueen(gameboard, input, a, y);
+  //printGameboard(gameboard, input);
   
-  for ( int i = 0; i < input; i++){
-    createQueen(gameboard, input, i, y);
-    for (int a = 1; a < input; a++){
-      for (int b = 0; b < input; b++){
-	valid = checkMove(gameboard, b, a);
-	if (valid) {
-	  createQueen(gameboard,input ,b ,a );
+  for (int i = 0; i < input; i++){			    // Go through the first line
+    createQueen(gameboard, input, i, y);		    // Create a queen on the first square
+    for (int b = 1; b < input; b++){			    // itterate through the y (y=1,y=2,y=3...)
+      for (int a = 1; a < input; a++){			    // itterate through the x (x'')
+	if (gameboard[a][b] == '-'){                        // Check if it is and empty square
+	  createQueen(gameboard, input, a, b);		    // Create queen on empty square
+	}
+	else if ( a > input ) {				    // if the whole row is full check the next row
+	  a = 0;					    // got to the next line and set the x value to zero
+	  b++;
+	  createQueen(gameboard, input, a, b);
+	}
+	else if ( b > input ) {				    // check if the y value is at the end
+	  break;					    // end the loop if it is
 	}
       }
-      break;
     }
+    break;
   }
+
+  
   printGameboard(gameboard, input);
+
+  checkQueens(gameboard, input);
 
   deleteGameboard(gameboard, input);
 
@@ -94,5 +103,24 @@ bool checkMove(char**gameboard,int x, int y){
   }
   else {
     return true;
+  }
+}
+
+void checkQueens(char**gameboard, int squares) {	    // Quick way to read output and checks
+  int queensfound(0);
+  for (int y = 0; y < squares; y++){
+    for (int x = 0; x < squares; x++){
+      if (gameboard[x][y] != 'X'){
+	queensfound++;
+	std::cout << "X: " << x << " Y: " << y << std::endl;
+      }
+    }
+  }
+  std::cout << "Total queens drawn: " << queensfound << std::endl;
+  if (queensfound < squares) {
+    std::cout << "Program Failed" << std::endl;
+  }
+  else{
+    std::cout << "Program Wroks" << std::endl;
   }
 }
